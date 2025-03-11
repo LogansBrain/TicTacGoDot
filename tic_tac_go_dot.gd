26368f52ac9f304cf364d8881fec5d32f52d37eb
LogansBrain <logansbrain@hotmail.com>
2025-03-11 08:52:53 -0500

    Added some comments
    The code is fairly easy to follow but I added some comments. The comments are not perfect but should allow for better understanding especially where I used "zero" value of a variable to reference its state while also holding place values. (and subtracting one after I knew that it was a non zero to have zero-"x" values.

--- tic_tac_go_dot.gd
+++ tic_tac_go_dot.gd
@@ -1,14 +1,18 @@
 extends Node2D
-#
+
+# These are the 2 symbols in the game that can be chosen for players tokens.
 const SYMBOL = ["X","O"]
 
 @onready var buttons = $GridContainer.get_children()
 
-var playersturn = true
-var computermoves = 0
-var current_player = 0
-var player_token
-var new_game_status = true
+var playersturn = true  # True if we are expecting user input. (including selecting token)
+var computermoves = 0   # Number of moves the computer has made
+var current_player = 0  # Current player (0 is first, 1 is second)
+var player_token        # Which token the player has selected to play
+var new_game_status = true  # If we are starting a new game, waiting for Token selection
+
+# pattern is an array of strings 9 characters in length that represent the squares of a board
+# the 3 '*' characters in each pattern represents a winning line with the 9 square board
 var pattern: Array[String] = ["***------",
 							 "---***---", 
 							 "------***", 
@@ -17,6 +21,14 @@ var pattern: Array[String] = ["***------",
 							 "--*--*--*",
 							 "*---*---*",
 							 "--*-*-*--"] 
+
+# new_game sets up the board and global variables to start a new game
+# sets new_game_status to true to await input choice of player token
+# disables all button controls on the page that should not function during this time
+# changes the status text to indicate waiting for token choice
+# sets the current player global variable to 0 (first player)
+# sets playersturn to true to await a user input
+# resets computers move count to 0
 
 func new_game():
 	var button_index = 0
@@ -39,23 +51,40 @@ func new_game():
 	playersturn = true
 	computermoves = 0
 
+# check_pattern checks the board for a winning pattern given the symbol passed
+# blank counts blank spaces in a winning pattern
+# matches counts number of matches in a winning pattern
+
 func check_pattern(check_symbol):
 
 	var blank
 	var matches
 	
+	# for each pattern in our array
 	for i in range(8):
+		# initialize the count of blanks and matches
 		blank=0
 		matches=0
+		# for each position in the string pattern
 		for j in range(9):
+			# check if the position of the board is matters to our winning pattern
 			if pattern[i][j] == "*":
+				# if the position is relevant check if it is occupied by the symbol we are checking
 				if buttons[j].text == check_symbol:
 					matches += 1
+				# or if the relevant position is blank
 				elif buttons[j].text == "":
 					blank = j+1  # add one to be used in verify
+		#if 2 marks from the same player and a blank are in the same line return that position
 		if (matches == 2) and (blank > 0):
 			return blank
-	return 0
+	return 0  # otherwise return 0, no winning place found
+
+# checkgameover checks the 8 possible winning lines to see if the same
+# player (symbol) occupies all 3 positions and returns True
+# or if the board is fully occupied it will also return True
+# full tracks if the board is full
+# matches tracks the number of matches within a winning pattern
 
 func checkgameover(symbol):
 
@@ -82,6 +111,12 @@ func checkgameover(symbol):
 			button.disabled=true
 		return true
 
+# the computer_move function determines a move for the computer.
+# the place variable tracks the best place to go
+# the done tracks if the search for place to go is done
+# move tracks the square that is the move to be made
+# try counts the number of tries the computer looks for a space to go
+
 func computer_move():
 
 	var place
@@ -106,23 +141,36 @@ func computer_move():
 	done = false
 	try = 0
 	while !done:
+		# increase try count
 		try += 1
+		# if move is > 0 then we have found a move
 		if move > 0:
-			place = move - 1
+			place = move - 1   # minus 1 because we use 0 to indicate no move
 		else:
-			if buttons[4].text == "":
+			if buttons[4].text == "":  # the computer will always try to take Center square
 				place = 4
 			else:
-				if (computermoves > 2) or (try > 20):
+				# after the computers first 2 moves or if the 50 attempts to
+				# take a preferred side or corner the computer will take a 
+				# completely random choice
+				if (computermoves > 2) or (try > 50):
 					place = int(round(randi_range(0, 8)))
 				else: 
+					# this is the logic for the computers 2nd move
+					# if the player is in the center the computer will prefer 
+					# an even numbered square (corner)
+					# technically the center is even also but the computer already
+					# tried for their first move as center, it is taken by this point
 					if buttons[4].text == SYMBOL[player_token]:
 						place = int(round(randi_range(0, 4))) * 2
 					else:
+						# otherwise an odd (side) square
 						place = int(round(randi_range(0, 4)) * 2) + 1
+		# if the square that we have chosen is blank then put a mark there
 		if buttons[place].text == "":
 			done = true
 			buttons[place].text = SYMBOL[player_token^1]
+	# check if the computers move was a winning move or filled the board.
 	checkgameover(SYMBOL[player_token^1])
 	playersturn=true
 	
@@ -135,8 +183,11 @@ func _ready():
 	new_game()
 
 func on_btnSquare_click(idx, clicked_button):
+	# when a player clicks the board,check if it is users turn.
 	if not playersturn:
 		return
+		
+	# if the new_game_status flag is set, draw a board to allow token choice	
 	if new_game_status:
 		if idx==3:
 			player_token=0
@@ -150,7 +201,10 @@ func on_btnSquare_click(idx, clicked_button):
 		if player_token==1:
 			computer_move()
 		return
-		
+	
+	# if it is the player turn and they have already selected a token
+	# check if the square selected is taken, if not give it to player
+	# check if the players move created a win or tie if not move the computer	
 	if clicked_button.text=="":
 		clicked_button.text = SYMBOL[player_token]
 		playersturn=false
